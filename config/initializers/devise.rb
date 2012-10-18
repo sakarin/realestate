fe49ec53cb5b71448953f2e897904c13faab78c2
sakarin <sakarin.my@gmail.com>
2012-10-18 11:05:44 +0700

    facebook auth done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -207,6 +207,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :facebook, '393441350728300', '8508c6a80e13e7c9e59ca60ef29f99e4'
+  #, :display => 'popup'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Realestate::Application.routes.draw do
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+             controllers: {omniauth_callbacks: "omniauth_callbacks"}
+
   get "home/index"
 
   devise_for :users
