80ab990fcd757c1d08b1331534c27cbd402958d2
sakarin <sakarin.my@gmail.com>
2013-03-04 05:59:24 +0700

    Fixing a typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ Realestate::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -31,7 +31,7 @@ Realestate::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :error
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
